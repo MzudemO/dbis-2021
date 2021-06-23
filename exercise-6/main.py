eba7d548cbd5dc5cd5dc6924f8bf9661c2b9dc1b
MzudemO <m.c.lahann@gmail.com>
2021-06-24 01:30:50 +0200

    rename function in main after refactor

--- exercise-6/main.py
+++ exercise-6/main.py
@@ -4,7 +4,7 @@ from ets import ETS
 if __name__ == "__main__":
     c = Connection()
     e = ETS(c)
-    e.drop_and_create_sales()
+    e.drop_and_create_schema()
     e.read_from_csv("sales.csv")
 
     e.cursor.close()
