0e6f562e496bd9657b8d737c78f355d92b858dcb
MzudemO <m.c.lahann@gmail.com>
2021-07-08 10:27:49 +0200

    ex 7

--- exercise-7/exercise-7.py
+++ exercise-7/exercise-7.py
@@ -4,9 +4,37 @@ def load_data_set():
     Returns:
         A data set: A list of transactions. Each transaction contains several items.
     """
-    data_set = [['1', '2', '5'], ['2', '4'], ['2', '3'],
-            ['1', '2', '4'], ['1', '3'], ['2', '3'],
-            ['1', '3'], ['1', '2', '3', '5'], ['1', '2', '3']]
+    data_set = [
+        ["1", "2", "5"],
+        ["2", "4"],
+        ["2", "3"],
+        ["1", "2", "4"],
+        ["1", "3"],
+        ["2", "3"],
+        ["1", "3"],
+        ["1", "2", "3", "5"],
+        ["1", "2", "3"],
+    ]
+    return data_set
+
+
+def data_set_from_txt():
+    """
+    Load a data set from a text file
+    File is expected to have one transaction per line, with identifiers separated by whitespace
+    Returns:
+        A data set: A list of transactions. Each transaction contains several items.
+    """
+    data_set = []
+    with open("transactions.txt") as f:
+        for line in f:
+            data_set.append(line.split(" ")[:-1])
+
+    max_t = 0
+    for t in data_set:
+        if len(t) > max_t:
+            max_t = len(t)
+    print(f"Longest transaction, largest possible frequent item set: {max_t}")
     return data_set
 
 
@@ -63,7 +91,7 @@ def create_Ck(Lksub1, k):
             l2 = list(list_Lksub1[j])
             l1.sort()
             l2.sort()
-            if l1[0:k-2] == l2[0:k-2]:
+            if l1[0 : k - 2] == l2[0 : k - 2]:
                 Ck_item = list_Lksub1[i] | list_Lksub1[j]
                 # pruning
                 if is_apriori(Ck_item, Lksub1):
@@ -116,54 +144,106 @@ def generate_L(data_set, k, min_support):
     Lksub1 = L1.copy()
     L = []
     L.append(Lksub1)
-    for i in range(2, k+1):
+    for i in range(2, k + 1):
+        print(i)
         Ci = create_Ck(Lksub1, i)
         Li = generate_Lk_by_Ck(data_set, Ci, min_support, support_data)
+        print(f"itemsets with {i} items: {len(Li)}")
         Lksub1 = Li.copy()
         L.append(Lksub1)
     return L, support_data
-
-
-def generate_big_rules(L, support_data, min_conf):
-    """
-    Generate big rules from frequent itemsets.
-    Args:
-        L: The list of Lk.
-        support_data: A dictionary. The key is frequent itemset and the value is support.
-        min_conf: Minimal confidence.
-    Returns:
-        big_rule_list: A list which contains all big rules. Each big rule is represented
-                       as a 3-tuple.
-    """
-    big_rule_list = []
-    sub_set_list = []
-    for i in range(0, len(L)):
-        for freq_set in L[i]:
-            for sub_set in sub_set_list:
-                if sub_set.issubset(freq_set):
-                    conf = support_data[freq_set] / support_data[freq_set - sub_set]
-                    big_rule = (freq_set - sub_set, sub_set, conf)
-                    if conf >= min_conf and big_rule not in big_rule_list:
-                        # print freq_set-sub_set, " => ", sub_set, "conf: ", conf
-                        big_rule_list.append(big_rule)
-            sub_set_list.append(freq_set)
-    return big_rule_list
 
 
 if __name__ == "__main__":
     """
     Test
     """
-    data_set = load_data_set()
-    L, support_data = generate_L(data_set, k=3, min_support=0.2)
-    big_rules_list = generate_big_rules(L, support_data, min_conf=0.7)
-    for Lk in L:
-        print("="*50)
-        print ("frequent " + str(len(list(Lk)[0])) + "-itemsets\t\tsupport")
-        print("="*50)
+    data_set = data_set_from_txt()
+    L, support_data = generate_L(data_set, k=26, min_support=0.01)
+    for Lk in L[:-1]:
+        print("=" * 50)
+        print("frequent " + str(len(list(Lk)[0])) + "-itemsets\t\tsupport")
+        print("=" * 50)
         for freq_set in Lk:
             print(freq_set, support_data[freq_set])
 
-    print ("Big Rules")
-    for item in big_rules_list:
-        print (item[0], "=>", item[1], "conf: ", item[2])
+
+"""
+    OUTPUT
+"""
+
+"""
+2
+itemsets with 2 items: 11
+3
+itemsets with 3 items: 1
+4
+itemsets with 4 items: 0
+5
+itemsets with 5 items: 0
+6
+itemsets with 6 items: 0
+7
+itemsets with 7 items: 0
+8
+itemsets with 8 items: 0
+9
+itemsets with 9 items: 0
+10
+itemsets with 10 items: 0
+11
+itemsets with 11 items: 0
+12
+itemsets with 12 items: 0
+13
+itemsets with 13 items: 0
+14
+itemsets with 14 items: 0
+15
+itemsets with 15 items: 0
+16
+itemsets with 16 items: 0
+17
+itemsets with 17 items: 0
+18
+itemsets with 18 items: 0
+19
+itemsets with 19 items: 0
+20
+itemsets with 20 items: 0
+21
+itemsets with 21 items: 0
+22
+itemsets with 22 items: 0
+23
+itemsets with 23 items: 0
+24
+itemsets with 24 items: 0
+25
+itemsets with 25 items: 0
+26
+itemsets with 26 items: 0
+==================================================
+frequent 1-itemsets		support
+==================================================
+... trunc.
+==================================================
+frequent 2-itemsets		support
+==================================================
+frozenset({'829', '368'}) 0.0127
+frozenset({'704', '39'}) 0.0139
+frozenset({'217', '283'}) 0.0104
+frozenset({'390', '722'}) 0.0103
+frozenset({'825', '39'}) 0.0151
+frozenset({'825', '704'}) 0.0135
+frozenset({'682', '368'}) 0.0128
+frozenset({'390', '227'}) 0.0101
+frozenset({'217', '346'}) 0.0149
+frozenset({'789', '829'}) 0.0119
+frozenset({'692', '368'}) 0.0114
+==================================================
+frequent 3-itemsets		support
+==================================================
+frozenset({'825', '704', '39'}) 0.0128
+==================================================
+"""
